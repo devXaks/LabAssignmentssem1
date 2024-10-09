6388865d976913a6c6af29d0d211d562da72f120
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-10-09 18:31:52 +0530

    updated files

--- LAB3/assignment1.c
+++ LAB3/assignment1.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 void printNumberInWords(int n) {
-    char *single_digits[] = { "zero", "one", "two", "three", "four", 
+    char single_digits[] = { "zero", "one", "two", "three", "four", 
                               "five", "six", "seven", "eight", "nine" };
-    char *two_digits[] = { "", "ten", "eleven", "twelve", "thirteen", "fourteen", 
+    char two_digits[] = { "", "ten", "eleven", "twelve", "thirteen", "fourteen", 
                            "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
-    char *tens_multiple[] = { "", "", "twenty", "thirty", "forty", "fifty", 
+    char tens_multiple[] = { "", "", "twenty", "thirty", "forty", "fifty", 
                               "sixty", "seventy", "eighty", "ninety" };
-    char *tens_power[] = { "hundred", "thousand" };
+    char tens_power[] = { "hundred", "thousand" };
 
     if (n == 0) {
         printf("zero");
