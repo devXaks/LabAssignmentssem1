76983c515abb45926dafac5ce12d0a15e4fe74ae
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-10-30 12:45:58 +0530

    udpated files

---
+++ test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+int x,y,z;
+char a,b;
+float f;
+x = 10;
+y = 11;
+
+switch (x<y)
+{
+case 2:
+printf("Case 1");
+    break;
+case 0:
+printf("Case 0");
+break;
+
+
+default:
+    break;
+}
+    
+    return 0;
+}
