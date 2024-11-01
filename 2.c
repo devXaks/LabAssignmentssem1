584a2098e83bc485cbab5573a532e7b216db386c
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-11-01 12:14:39 +0530

    updated files

---
+++ 2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+int main(){
+    // char str1[80];
+    // gets(str1);
+    // printf("%s",str1); 
+
+    // char s = 'A';
+    // putchar(s);
+
+    struct student {
+    
+    }s1,s2;
+
+    char str1[10] = "Akshat ";
+    char str2[10] = "Trip";
+    strcat(str1,str2);
+    printf("%s",str1);
+    return 0;
+}
