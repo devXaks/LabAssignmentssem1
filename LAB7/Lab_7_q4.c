61d76316b110223f9fa5361e9a69299b5e5fc0a2
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-11-13 01:51:08 +0530

    updated files

--- LAB7/Lab_7_q4.c
+++ LAB7/Lab_7_q4.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+int sod(int num1){
+    int k,d;
+    k = num1%10;
+    d = num1/10;
+    if(d == 0){
+        return k;
+    }
+    return k+sod(d);
+
+
+}
 
 int main(){
+
+    printf("%d",sod(5050));
+    
     
     return 0;
 }
